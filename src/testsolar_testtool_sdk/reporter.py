0517226bafd26a29953cac9bb889ae18526c5bd3
ambzhang <ambzhang@tencent.com>
2024-07-01 23:23:38 +0800

    1

--- src/testsolar_testtool_sdk/reporter.py
+++ src/testsolar_testtool_sdk/reporter.py
@@ -17,7 +17,6 @@ MAGIC_NUMBER = 0x1234ABCD
 # 跟TestSolar uniSDK约定的管道上报文件描述符号
 PIPE_WRITER = 3
 
-
 class Reporter:
     def __enter__(self) -> 'Reporter':
         return self
@@ -34,7 +33,7 @@ class Reporter:
         else:
             self.pipe_io = os.fdopen(PIPE_WRITER, "wb")
 
-        logging.debug(f"Reporter initialized with pipe_io: {self.pipe_io}")
+        print(f"====!!!! Reporter initialized with pipe_io: {self.pipe_io}")
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         self.close()
@@ -49,7 +48,7 @@ class Reporter:
 
     def close(self) -> None:
         if self.pipe_io:
-            logging.debug("Closing pipe_io")
+            print("======Closing pipe_io=====")
             self.pipe_io.close()
             self.pipe_io = None
 
@@ -66,6 +65,6 @@ class Reporter:
         # 将 JSON 数据本身写入管道
         self.pipe_io.write(data)
 
-        logging.debug(f"Sending {length} bytes to pipe {PIPE_WRITER}")
+        print(f"====!!!! Sending {length} bytes to pipe {PIPE_WRITER}")
 
         self.pipe_io.flush()
