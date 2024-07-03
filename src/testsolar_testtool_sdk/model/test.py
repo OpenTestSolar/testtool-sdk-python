ad50b68f3ddf9be69ac7a5dab395006ce12891c4
asiazhang <asiazhang2002@gmail.com>
2024-07-03 15:28:18 +0800

    允许TestCase进行比较，方便去重复

--- src/testsolar_testtool_sdk/model/test.py
+++ src/testsolar_testtool_sdk/model/test.py
@@ -3,9 +3,21 @@ from typing import Dict
 from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass()
 class TestCase:
     __test__ = False
 
     Name: str
     Attributes: Dict[str, str] = field(default_factory=dict)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.Name == other.Name
+        else:
+            return False
+
+    def __hash__(self):
+        """
+        Name相同认为是同一个用例
+        """
+        return hash(self.Name)
