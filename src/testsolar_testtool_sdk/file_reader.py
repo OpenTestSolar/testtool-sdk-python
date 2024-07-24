310a404f58681a21e708f2d638591688ecac2fc5
asiazhang <asiazhang2002@gmail.com>
2024-07-24 18:14:11 +0800

    load结果不附加result.json

--- src/testsolar_testtool_sdk/file_reader.py
+++ src/testsolar_testtool_sdk/file_reader.py
@@ -5,12 +5,12 @@ from typing import Dict, Any
 from .model.load import LoadResult, deserialize_load_result
 from .model.test import TestCase
 from .model.testresult import TestResult, deserialize_test_result
-from .reporter import LOAD_RESULT_FILE_NAME, digest_file_name
+from .reporter import digest_file_name
 
 
 # 从管道读取加载结果，仅供单元测试使用
 def read_file_load_result(file_report_path: Path) -> LoadResult:
-    result_path = file_report_path / LOAD_RESULT_FILE_NAME
+    result_path = file_report_path
     with result_path.open() as f:
         data_dict: Dict[str, Any] = json.loads(f.read())
 
