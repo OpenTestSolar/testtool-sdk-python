abbc505296b968b308fcbeaf3039279cbc94c4cb
asiazhang <asiazhang2002@gmail.com>
2024-09-02 20:34:21 +0800

    使用uv替代pdm

--- check.sh
+++ check.sh
@@ -2,9 +2,30 @@
 
 set -exu -o pipefail
 
-pdm run ruff format src
-pdm run ruff format tests
-pdm run ruff check src
-pdm run ruff check tests
-pdm run mypy src --strict
-pdm run pytest tests --durations=5 --cov=. --cov-fail-under=90 --cov-branch --cov-report term
+if [[ -z "${GITHUB_ACTIONS+x}" ]]; then
+  echo "GITHUB_ACTIONS environment variable is not set.Use local mode."
+  uv sync --all-extras --dev
+  uv run ruff format src
+  uv run ruff format tests
+  uv run ruff check src
+  uv run ruff check tests
+  uv run mypy src/ --strict
+  uv run pytest tests --durations=5 --cov=. --cov-fail-under=90 --cov-report term
+  uv export --no-dev --locked >requirements.txt
+else
+  echo "GITHUB_ACTIONS environment variable is set.Use CI mode."
+  uv sync --all-extras --dev
+
+  uv export --no-dev --locked >requirements.txt
+  # 检查是否有未提交的变化
+  if ! git diff-index HEAD --; then
+    echo "Check uncommit changes.Please run bash check.sh and commit again.Changed files:"
+    git diff --name-only
+    exit 1
+  fi
+
+  uv run ruff check src
+  uv run ruff check tests
+  uv run mypy src/ --strict
+  uv run pytest tests --durations=5 --cov=. --cov-fail-under=90 --cov-report term
+fi
