dfac52eccda0f20bd668161a8a3aee61d4c2cc03
asiazhang <asiazhang2002@gmail.com>
2024-09-02 20:36:52 +0800

    remove unused code

--- check.sh
+++ check.sh
@@ -11,19 +11,9 @@ if [[ -z "${GITHUB_ACTIONS+x}" ]]; then
   uv run ruff check tests
   uv run mypy src/ --strict
   uv run pytest tests --durations=5 --cov=. --cov-fail-under=90 --cov-report term
-  uv export --no-dev --locked >requirements.txt
 else
   echo "GITHUB_ACTIONS environment variable is set.Use CI mode."
   uv sync --all-extras --dev
-
-  uv export --no-dev --locked >requirements.txt
-  # 检查是否有未提交的变化
-  if ! git diff-index HEAD --; then
-    echo "Check uncommit changes.Please run bash check.sh and commit again.Changed files:"
-    git diff --name-only
-    exit 1
-  fi
-
   uv run ruff check src
   uv run ruff check tests
   uv run mypy src/ --strict
